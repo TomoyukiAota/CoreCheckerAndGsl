1232833d3959d6ed31063deed9ef3fd6337e28e5
Tomoyuki Aota <tomoyukia@gmail.com>
2018-03-07 00:56:03 +0900

    Add code sample for gsl::span<T>

--- CoreCheckerAndGsl/Source.cpp
+++ CoreCheckerAndGsl/Source.cpp
@@ -1,8 +1,13 @@
-//#include <gsl/gsl>     // Directly including gsl/gsl results in warnings from GSL code.
 #include "gsl_wrapper.h" // Including gsl/gsl through gsl_wrapper.h disables warnings from GSL code.
+//#include <gsl/gsl>     // Directly including gsl/gsl results in warnings from GSL code.
+
+#include <vector>
 
 int main()
 {
 	const int integer = 1;
 	const size_t size = gsl::narrow<size_t>(integer);
+
+    std::vector<int> vec{ 1, 2, 3, 4 };
+    const gsl::span<int> s{ vec };
 }
