87a020a02823b88b11dfc5b597ac0942e1033dc8
TomoyukiAota <tomoyukia@gmail.com>
2018-05-07 18:40:50 +0900

    Enable #pragma warning only in VS2017.

--- include/gsl_wrapper.h
+++ include/gsl_wrapper.h
@@ -1,10 +1,16 @@
 #ifdef _MSC_VER
+#if _MSC_VER >= 1910
 #include <CppCoreCheck/warnings.h>
 #pragma warning(disable: ALL_CPPCORECHECK_WARNINGS)
+#endif // _MSC_VER >= 1910
 #endif // _MSC_VER
+
 
 #include <gsl/gsl>
 
+
 #ifdef _MSC_VER
+#if _MSC_VER >= 1910
 #pragma warning(default: ALL_CPPCORECHECK_WARNINGS)
+#endif // _MSC_VER >= 1910
 #endif // _MSC_VER
